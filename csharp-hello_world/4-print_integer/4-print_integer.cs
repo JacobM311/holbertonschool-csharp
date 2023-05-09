ad3d8059ca60c64f521b316520a0bdac29e1f1ae
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-09 15:23:19 -0500

    task 4

---
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _4_print_integer
+{
+    class Program
+    {
+	    static void Main(string[] args)
+	    {
+		    int number = 972;
+		    string y = "Mission Street";
+
+            Console.WriteLine(string.Format("{0} {1}", number, y));
+	    }
+    }
+}
