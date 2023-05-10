f6282bd9bebb421c542d8c4a05da1d54f2b1c691
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-09 19:13:58 -0500

    task 5

--- csharp-hello_world/5-print_float/5-print_float.cs
+++ csharp-hello_world/5-print_float/5-print_float.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-
 class Program
 {
     static void Main(string[] args)
     {
         float number = 3.14159f;
-        Console.WriteLine(string.Format("Float: {0}", number));
+        string formattedNumber = number.ToString("F2");
+        Console.WriteLine(string.Format("Float: {0}", formattedNumber));
     }
 }
