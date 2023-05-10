6692243c792397392263e533eba001d4a026d48d
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-09 19:58:45 -0500

    task 6

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -6,6 +6,6 @@ class Program
 	{
 		string str = "Holberton School";
 		Console.WriteLine(string.Format("{0}{1}", str, string.Join("", Enumerable.Repeat(str, 2))));
-        Console.WriteLine(str.Substring(0, 9) + Environment.NewLine);
+        Console.WriteLine(str.Substring(0, 9));
     }
 }
