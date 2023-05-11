ad1c6aa03d58ecdfdddcd79e91791eb4f5faab0d
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-10 23:51:21 -0500

    task 2

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 97;
+
+        while (i <= 122)
+        {
+            Console.Write((char)i);
+            i++;
+        }
+    }
+}
