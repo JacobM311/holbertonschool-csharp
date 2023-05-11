f0c15594288082b280d7a432afd2b7a825df7578
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-10 20:05:12 -0500

    innit

---
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+        if (number == 0)
+            Console.WriteLine("is zero");
+        if (number > 0)
+            Console.WriteLine("is positive");
+        if (number < 0)
+            Console.WriteLine("is negative");
+    }
+}
