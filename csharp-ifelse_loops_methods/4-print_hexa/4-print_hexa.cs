52adf2e53a29bff8c5a6ad74c87d3220f1ce5893
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-11 09:31:06 -0500

    task 4

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int x = 0; x <= 98; x++)
+        {
+            Console.Write(string.Format("{0} = 0x{1}{2}", x, x.ToString("X"), Environment.NewLine));
+        }
+    }
+}
