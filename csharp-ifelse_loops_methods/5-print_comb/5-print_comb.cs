a636db06215f8dd890c65f186c048f20d9198391
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-11 12:14:27 -0500

    task 5

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program 
+{         
+    static void Main(string[] args)
+    {
+
+        for (int number = 0; number <= 99; number++)
+        {
+            if (number != 99)
+            {
+                Console.Write(number.ToString("D2") + ", ");
+            }
+            else
+            {
+                Console.Write("99");
+            }
+        }
+    }
+}
