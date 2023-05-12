7dcf29c10e8c002e08f6fd24440c3ab10c5a218e
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-11 19:10:45 -0500

    task 12

---
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program 
+{         
+    static void Main(string[] args)
+    {
+        for (int i = 1; i < 100; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.Write("Fizz ");
+                continue;
+            }
+            if (i % 5 == 0)
+            {
+                Console.Write("Buzz ");
+                continue;
+            }
+            if (i % 15 == 0)
+            {
+                Console.Write("FizzBuzz ");
+                continue;
+            }
+
+            Console.Write(i + " ");
+        }
+    }
+}
