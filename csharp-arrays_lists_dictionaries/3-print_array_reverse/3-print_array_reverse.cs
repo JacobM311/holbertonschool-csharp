d49672467625fea8ee9142dcc99324f92084d734
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-12 15:40:22 -0500

    task 3

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null)
+        {
+            Console.WriteLine();
+            return;
+        }
+
+        if (array.Length == 0)
+        {
+            Console.WriteLine();
+            return;
+        }
+
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
