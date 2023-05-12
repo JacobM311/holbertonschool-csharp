6c35a9c557b268a1d612aa96ee4f0290d1fa955c
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-12 18:03:29 -0500

    task 4

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int> intList = new List<int>();
+
+        for (int i = 0; i < size; i++)
+        {
+            intList.Add(i);
+            Console.Write(i + " ");
+        }
+
+        Console.WriteLine();
+
+        return intList;
+    }
+}
