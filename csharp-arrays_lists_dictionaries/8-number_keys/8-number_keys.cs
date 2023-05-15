37b01019fd43597bd57543e0efe668143cee28fd
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-15 18:38:31 -0500

    task 8

--- csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -13,17 +13,3 @@ public class Dictionary
         return count;
     }
 }
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Dictionary<string, string> myDict = new Dictionary<string, string>();
-
-        myDict.Add("language", "C");
-        myDict.Add("track", "low level");
-        myDict.Add("school", "Holberton");
-
-        Console.WriteLine("Number of keys: {0}", Dictionary.NumberOfKeys(myDict));
-    }
-}
