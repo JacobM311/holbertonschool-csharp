50bb603741093c9c3114fe0a8a613557bc0db6e3
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-15 21:36:22 -0500

    task 0

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> uniqueIntegers = new HashSet<int>();
+        int sum = 0;
+
+        foreach (int num in myList)
+        {
+            if (!uniqueIntegers.Contains(num))
+            {
+                uniqueIntegers.Add(num);
+                sum += num;
+            }
+        }
+
+        return sum;
+    }
+}
