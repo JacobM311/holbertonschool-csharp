52b998c058d8e6e363415b8e2c4f4fb72b9eb5aa
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-15 23:37:03 -0500

    task 5

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> myList = new LinkedList<int>();
+
+        if (size <= 0)
+            return myList;
+
+        for (int i = 0; i < size; i++)
+        {
+            myList.AddLast(i);
+            Console.Write(i + " ");
+        }
+
+        Console.WriteLine();
+        return myList;
+    }
+}
