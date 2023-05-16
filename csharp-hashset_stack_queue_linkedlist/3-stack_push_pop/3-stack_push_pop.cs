d24ca00343fd22cb58e9525366e796e0780ea313
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-15 22:33:58 -0500

    task 3

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: " + aStack.Count);
+
+        if (aStack.Count > 0)
+            Console.WriteLine("Top item: " + aStack.Peek());
+        else
+            Console.WriteLine("Stack is empty");
+
+        Console.WriteLine("Stack contains " + search + ": " + aStack.Contains(search));
+
+        if (aStack.Contains(search))
+        {
+            Stack<string> tempStack = new Stack<string>();
+            while (aStack.Pop() != search) { }
+        }
+
+        aStack.Push(newItem);
+
+        return aStack;
+    }
+}
