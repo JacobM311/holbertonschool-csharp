c5498958b1980eca60506d716c106561cbf4180e
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-16 15:17:57 -0500

    task 9

--- csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.First == null)
+            return 0;
+        
+        int value = myLList.First.Value;
+        myLList.RemoveFirst();
+        return value;
+    }
+}
