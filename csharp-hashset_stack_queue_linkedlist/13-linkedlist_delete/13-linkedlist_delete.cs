bca955b5904eae5c15b494f78e2acb2a5b2d4005
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-16 15:45:37 -0500

    task 13

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        if (index < 0 || myLList.First == null)
+        {
+            return; // Index out of bounds or list is empty
+        }
+
+        if (index == 0)
+        {
+            myLList.RemoveFirst();
+            return;
+        }
+
+        int currentIndex = 0;
+        LinkedListNode<int> current = myLList.First;
+
+        while (current != null)
+        {
+            if (currentIndex == index)
+            {
+                myLList.Remove(current);
+                return;
+            }
+            currentIndex++;
+            current = current.Next;
+        }
+    }
+}
