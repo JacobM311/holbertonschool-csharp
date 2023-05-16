899ec3ed7442a6b4c6f163339526e0f534b392ae
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-15 23:32:46 -0500

    task 4

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: " + aQueue.Count);
+
+        if (aQueue.Count > 0)
+            Console.WriteLine("First item: " + aQueue.Peek());
+        else
+            Console.WriteLine("Queue is empty");
+
+        aQueue.Enqueue(newItem);
+
+        Console.WriteLine("Queue contains \"" + search + "\": " + aQueue.Contains(search));
+
+        if (aQueue.Contains(search))
+        {
+            while (aQueue.Dequeue() != search) { }
+        }
+
+        return aQueue;
+    }
+}
