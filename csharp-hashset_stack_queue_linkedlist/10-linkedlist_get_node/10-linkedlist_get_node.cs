e2b03f8e3136b14e7ffc3189bbe8a8127b6a2a5c
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-16 15:23:45 -0500

    task 10

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int index = 0;
+        foreach (int item in myLList)
+        {
+            if (index == n)
+            {
+                return item;
+            }
+            index++;
+        }
+        return 0;
+    }
+}
