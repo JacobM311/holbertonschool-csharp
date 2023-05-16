92eb493549c8023ed20784b9c53f0f91e2dc6280
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-15 22:24:43 -0500

    task 2

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> set1 = new HashSet<int>(list1);
+        HashSet<int> set2 = new HashSet<int>(list2);
+
+        List<int> result = new List<int>();
+
+        foreach (int num in set1)
+        {
+            if (!set2.Contains(num))
+            {
+                result.Add(num);
+            }
+        }
+
+        foreach (int num in set2)
+        {
+            if (!set1.Contains(num))
+            {
+                result.Add(num);
+            }
+        }
+
+        result.Sort();
+
+        return result;
+    }
+}
