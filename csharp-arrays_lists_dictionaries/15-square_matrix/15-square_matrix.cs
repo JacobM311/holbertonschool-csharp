de4b275165a0cf1b30967e14e8665e3eea9b9949
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-15 20:25:07 -0500

    task 15

---
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,21 @@
+﻿public class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int rows = myMatrix.GetLength(0);
+        int columns = myMatrix.GetLength(1);
+        int[,] squaredMatrix = new int[rows, columns];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                int value = myMatrix[i, j];
+                int squaredValue = value * value;
+                squaredMatrix[i, j] = squaredValue;
+            }
+        }
+
+        return squaredMatrix;
+    }
+}
