5e0fbe51f8dfe6087b752c47c7995f4ed55a253a
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-16 22:31:23 -0500

    task 1

---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System;
+
+public class Int
+{
+    public static void divide(int a, int b)
+    {
+        int result = 0;
+        try
+        {
+            result = a / b;
+            Console.Write(String.Format("{0} / {1} = ", a, b));
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+            Console.Write(String.Format("{0} / {1} = ", a, b));
+        }
+        finally
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
