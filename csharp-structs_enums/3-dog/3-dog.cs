168609c2962500976ac1e1b97dce1dcb0664f23b
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-18 10:15:13 -0500

    task 3

--- csharp-structs_enums/3-dog/3-dog.cs
+++ csharp-structs_enums/3-dog/3-dog.cs
@@ -28,15 +28,3 @@ public struct Dog
         return $"Dog Name: {name}\nAge: {age}\nOwner: {owner}\nRating: {rating}";
     }
 }
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Dog poppy;
-
-        poppy = new Dog("Poppy", 0.7f, "Kris", Rating.Excellent);
-
-        Console.WriteLine(poppy.ToString());
-    }
-}
