882faaece15241a5e77af24d721d20c04f34f959
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-18 10:13:50 -0500

    task 3

---
+++ csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+
+    public override string ToString()
+    {
+        return $"Dog Name: {name}\nAge: {age}\nOwner: {owner}\nRating: {rating}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog poppy;
+
+        poppy = new Dog("Poppy", 0.7f, "Kris", Rating.Excellent);
+
+        Console.WriteLine(poppy.ToString());
+    }
+}
