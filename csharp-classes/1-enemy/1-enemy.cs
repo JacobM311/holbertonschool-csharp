f5a9988da624f92710d2da1a08ad7c9acbb1270b
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-20 17:37:01 -0500

    task 2

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/// <summary>
+/// The Enemies namespace.
+/// </summary>
+namespace Enemies
+{
+    /// <summary>
+    /// Class Zombie.
+    /// Represents a zombie enemy in the game.
+    /// </summary>
+    public class Zombie
+    {
+        public int health;
+
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Enemies.Zombie newObject = new Enemies.Zombie();
+
+        Console.WriteLine("newObject is of type {0} and has a total of {1} health", newObject.GetType(), newObject.health);
+    }
+}
