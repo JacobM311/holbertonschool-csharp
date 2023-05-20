825a365a75cf265af633ff39caf8cad6d1a67519
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-20 18:35:09 -0500

    task 2

--- csharp-classes/1-enemy/1-enemy.cs
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -20,13 +20,3 @@ namespace Enemies
 
     }
 }
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Enemies.Zombie newObject = new Enemies.Zombie();
-
-        Console.WriteLine("newObject is of type {0} and has a total of {1} health", newObject.GetType(), newObject.health);
-    }
-}
