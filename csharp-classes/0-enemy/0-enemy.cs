d28e0752953b9367291408f88613377b25527812
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-20 16:55:00 -0500

    innit

--- csharp-classes/0-enemy/0-enemy.cs
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 
+/// <summary>
+/// The Enemies namespace.
+/// </summary>
 namespace Enemies
 {
+    /// <summary>
+    /// Class Zombie.
+    /// Represents a zombie enemy in the game.
+    /// </summary>
     public class Zombie
     {
-
     }
 }
