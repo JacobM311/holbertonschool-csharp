35b87854b98c468720dc0acb5e1b9a554f082882
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-21 01:08:09 -0500

    task 5

---
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Enemies
+{
+    public class Zombie
+    {
+        private int health;
+        private string name = "(No name)";
+
+        public Zombie()
+        {
+            this.health = 0;
+        }
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                this.health = value;
+            }
+        }
+        public int GetHealth()
+        {
+            return this.health;
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public override string ToString()
+        {
+            return $"Zombie Name: {this.Name} / Total Health: {this.GetHealth()}";
+        }
+    }
+}
