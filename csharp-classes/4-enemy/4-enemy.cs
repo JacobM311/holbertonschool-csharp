e99f21eb62cd636557ac91ca201a65f368821ac3
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-21 00:54:26 -0500

    task 4

---
+++ csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Enemies
+{
+    public class Zombie
+    {
+        private int health;
+        private string name = "(no name)";
+
+        public Zombie()
+        {
+            this.health = 0;
+        }
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                this.health = value;
+            }
+        }
+        public int GetHealth()
+        {
+            return this.health;
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public override string ToString()
+        {
+            return $"Zombie Name: {this.Name} / Total Health: {this.GetHealth()}";
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Enemies.Zombie newObject1;
+        Enemies.Zombie newObject2;
+
+        try
+        {
+            newObject1 = new Enemies.Zombie(10);
+            newObject1.Name = "Ed";
+            Console.WriteLine("{0} is of type {1} and has a total of {2} health", newObject1.Name, newObject1.GetType(), newObject1.GetHealth());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        try
+        {
+            newObject2 = new Enemies.Zombie(98);
+            Console.WriteLine("{0} is of type {1} and has a total of {2} health", newObject2.Name, newObject2.GetType(), newObject2.GetHealth());
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
