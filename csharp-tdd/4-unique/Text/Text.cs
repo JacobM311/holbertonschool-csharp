f14e6aca00056123d0473d4f06153edc3b422c50
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-22 04:44:58 -0500

    task 4

---
+++ csharp-tdd/4-unique/Text/Text.cs
@@ -0,0 +1,32 @@
+﻿namespace Text
+{
+    public class Str
+    {
+        public static int UniqueChar(string s)
+        {
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (charCount.ContainsKey(s[i]))
+                {
+                    charCount[s[i]]++;
+                }
+                else
+                {
+                    charCount[s[i]] = 1;
+                }
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (charCount[s[i]] == 1)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
