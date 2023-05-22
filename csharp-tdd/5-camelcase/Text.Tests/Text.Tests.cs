cf5e1eee0ec082094656aa14587e891676376d77
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-22 08:30:14 -0500

    task 5

--- csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
+++ csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -1,15 +1,33 @@
-namespace Text.Tests;
+using NUnit.Framework;
+using Text;
 
-public class Tests
+namespace Text.Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
 
-    [Test]
-    public void Test1()
+    public class Tests
     {
-        Assert.Pass();
+        [Test]
+        public void Test_EmptyString()
+        {
+            Assert.AreEqual(0, Str.CamelCase(""));
+        }
+
+        [Test]
+        public void Test_SingleWord()
+        {
+            Assert.AreEqual(1, Str.CamelCase("hello"));
+        }
+
+        [Test]
+        public void Test_TwoWords()
+        {
+            Assert.AreEqual(2, Str.CamelCase("helloWorld"));
+        }
+
+        [Test]
+        public void Test_MultipleWords()
+        {
+            Assert.AreEqual(4, Str.CamelCase("helloWorldCamelCase"));
+        }
     }
 }
