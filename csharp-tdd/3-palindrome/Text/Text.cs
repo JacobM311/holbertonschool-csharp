df0d11e2c50a6ec9132d2aad49e669e335299400
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-22 01:54:42 -0500

    task 3

---
+++ csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Text
+{
+    public class Str
+    {
+        public static bool IsPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return true;
+            }
+
+            string input = Regex.Replace(s.ToLower(), "[^a-z]", "");
+
+            char[] array = input.ToCharArray();
+            Array.Reverse(array);
+
+            return new string(array) == input;
+        }
+    }
+}
