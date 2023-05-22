1a671bbac80c204e4506a2a327fa46f7566db965
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-22 06:23:57 -0500

    task 5

---
+++ csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Text
+{
+    public class Class1
+    {
+        int words = 1;
+
+        public static int CamelCase(string s)
+        {
+            for(int i = 0; i < s.Length; i++)
+            {
+                if(Char.IsUpper(s[i]))
+                {
+                    words++;
+                }
+
+            }
+        }
+    }
+}
