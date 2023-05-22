6e84ed202d331e89e23bad3340232ff7e597dfa1
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-22 11:13:45 -0500

    task 0

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Obj
+{
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj.GetType() == typeof(int))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var a = 10;
+        var b = new List<int>();
+
+        if (Obj.IsOfTypeInt(a))
+            Console.WriteLine("{0} is of type int", nameof(a));
+        else
+            Console.WriteLine("{0} is NOT of type int", nameof(a));
+
+        if (Obj.IsOfTypeInt(b))
+            Console.WriteLine("{0} is of type int", nameof(b));
+        else
+            Console.WriteLine("{0} is NOT of type int", nameof(b));
+    }
+}
