e58e8897d3e1ad7a7f577312cb22247c54bf6c39
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-24 16:54:17 -0500

    task 7

---
+++ csharp-inheritance/7-shape/7-shape.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+
+public class Shape
+{
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+public class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    public int Width
+    {
+        get { return width; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            else
+                width = value;
+        }
+    }
+
+    public int Height
+    {
+        get { return height; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            else
+                height = value;
+        }
+    }
+    public new int Area()
+    {
+        return Width * Height;
+    }
+    public override string ToString()
+    {
+        return $"[Rectangle] {width} / {height}";
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Rectangle aRect = new Rectangle();
+
+        aRect.Width = 7;
+        aRect.Height = 4;
+
+        Console.WriteLine("Area: {0}", aRect.Area());
+        Console.WriteLine(aRect.ToString());
+    }
+}
