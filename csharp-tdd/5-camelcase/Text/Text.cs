697dbdffc8fa7f0aa0a1c7ec01f38bae015a897b
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-25 11:03:53 -0500

    task 5

--- csharp-tdd/5-camelcase/Text/Text.cs
+++ csharp-tdd/5-camelcase/Text/Text.cs
@@ -1,31 +1,33 @@
-﻿using System.Collections.Generic;
-using System;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Text
 {
+	/// <summary>have multiple methods for manage strings</summary>
     public class Str
     {
+		/// <summary>Check the number of words in the string.</summary>
+		/// <returns>The number of words in the string.</returns>
+		public static int CamelCase(string s)
+		{
+			if (s == null || s.Length == 0)
+				return 0;
 
-        public static int CamelCase(string s)
-        {
-            int words = 1;
+			List<string> words = new List<string>();
+			string word = "";
 
-            if(s.Length == 0)
-            {
-                return 0;
-            }
-            else
-            {
-                for(int i = 0; i < s.Length; i++)
-                {
-                    if(Char.IsUpper(s[i]))
-                    {
-                        words++;
-                    }
-
-                }
-            }
-            return words;
-        }
+			for (int i = 0; i < s.Length; i++)
+			{
+				if (s[i] >= 'A' && s[i] <= 'Z')
+				{
+					words.Add(word);
+					word = "" + s[i];
+				}
+				else
+					word += s[i];
+			}
+			words.Add(word);
+			return words.Count;
+		}
     }
 }
