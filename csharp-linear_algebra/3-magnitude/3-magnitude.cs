2562633cf3b3394d15cf42c085dec881849cadf0
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-28 18:53:25 -0500

    task 3

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _3_magnitude
+{
+    class VectorMath
+    {
+        public static double Magnitude(double[] vector)
+        {
+            if (vector.Length < 2 || vector.Length > 3)
+            {
+                return -1;
+            }
+
+            double sumOfSquares = 0;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                sumOfSquares += Math.Pow(vector[i], 2);
+            }
+
+            double magnitude = Math.Sqrt(sumOfSquares);
+
+            return Math.Round(magnitude, 2);
+        }
+    }
+}
