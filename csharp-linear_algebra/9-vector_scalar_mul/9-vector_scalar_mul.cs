43769d0f91c0ce6fe923961891560f01960ac814
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-29 03:46:09 -0500

    task 9

---
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        if (vector.Length < 2 || vector.Length > 3)
+        {
+            return new double[] { -1 };
+        }
+
+        double[] sumVector = new double[vector1.Length];
+        for (int i = 0; i < vector1.Length; i++)
+        {
+            sumVector[i] = vector[i] * scalar;
+        }
+
+        return sumVector;
+    }
+}
