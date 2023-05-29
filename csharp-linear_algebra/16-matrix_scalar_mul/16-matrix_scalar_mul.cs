23534f4bab5443103bf5c5e399b80b6b1ca70393
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-29 05:52:14 -0500

    task 16

---
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class MatrixMath
+{
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        if (matrix.Length == 0 || (matrix.GetLength(0) != 2 && matrix.GetLength(0) != 3) 
+            || (matrix.GetLength(1) != 2 && matrix.GetLength(1) != 3))
+        {
+            return new double[,] { { -1 } };
+        }
+
+        double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                result[i, j] = matrix[i, j] * scalar;
+            }
+        }
+        return result;
+    }
+}
