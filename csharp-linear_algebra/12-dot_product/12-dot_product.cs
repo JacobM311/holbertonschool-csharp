78f60c130ea6f465f2edd3aee216ef15b049f56c
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-29 05:35:12 -0500

    task 12

---
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class VectorMath
+{
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length < 2 || vector1.Length > 3 || vector1.Length != vector2.Length)
+        {
+            return -1;
+        }
+
+        double dotProduct = 0;
+
+        for (int i = 0; i < vector1.Length; i++)
+        {
+            dotProduct += vector1[i] * vector2[i];
+        }
+
+        return dotProduct;
+    }
+}
