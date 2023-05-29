2f00cc6d5d9febe6b7e0c40634c0bb1e290189ff
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-29 05:46:25 -0500

    task 14

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -4,6 +4,7 @@ public class MatrixMath
 {
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
+        // Check if both matrices are 2D or 3D
         if ((matrix1.GetLength(0) != matrix2.GetLength(0)) || (matrix1.GetLength(1) != matrix2.GetLength(1))
             || (matrix1.GetLength(0) < 2 || matrix1.GetLength(0) > 3) || (matrix1.GetLength(1) < 2 || matrix1.GetLength(1) > 3))
         {
