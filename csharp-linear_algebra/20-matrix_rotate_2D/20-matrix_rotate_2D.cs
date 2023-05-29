903c742d27f3b59d782194cac8a5669531cdbc53
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-29 07:06:51 -0500

    task 20

---
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class MatrixMath
+{
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        {
+            return new double[,] { { -1 } };
+        }
+
+        double[,] result = new double[2, 2];
+        double cos = Math.Cos(angle);
+        double sin = Math.Sin(angle);
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                double x = matrix[i, j];
+                double y = matrix[j, i];
+
+                result[i, j] = x * cos - y * sin;
+                result[j, i] = x * sin + y * cos;
+            }
+        }
+
+        return result;
+    }
+}
