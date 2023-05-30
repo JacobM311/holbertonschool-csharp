e1b38b39e256a1707b9c28b490b4c44b6e888780
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-30 09:03:50 -0500

    task 2

---
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+public abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType()}";
+    }
+
+}
+
+public interface IInteractive
+{
+    void Interact();
+}
+
+public interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+public interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+public class Door : Base, IInteractive
+{
+    public Door(string name)
+    {
+        if (name == null)
+        {
+            name = "Door";
+        }
+        this.name = name;
+    }
+
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+
+}
