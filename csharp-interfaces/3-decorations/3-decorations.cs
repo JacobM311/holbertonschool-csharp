bda0523fa0068cac1add0a67ae81e4e5baf2f4bd
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-30 12:08:38 -0500

    task 3

--- csharp-interfaces/3-decorations/3-decorations.cs
+++ csharp-interfaces/3-decorations/3-decorations.cs
@@ -85,23 +85,3 @@ public class Decoration : Base, IInteractive, IBreakable
         }
     }
 }
-class Program
-{
-    static void Main(string[] args)
-    {
-        Decoration figurine = new Decoration("Expensive Figurine", isQuestItem: true);
-
-        Console.WriteLine(figurine.ToString());
-
-        figurine.Interact();
-        figurine.Break();
-        figurine.Break();
-
-        figurine.name = "Cheap Figurine";
-        figurine.durability = 2;
-        figurine.isQuestItem = false;
-
-        figurine.Interact();
-        figurine.Break();
-    }
-}
