6f6b7a4fa5ca27468510a21e010113309b3845c3
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-30 11:39:28 -0500

    task 3

--- csharp-interfaces/3-decorations/3-decorations.cs
+++ csharp-interfaces/3-decorations/3-decorations.cs
@@ -84,17 +84,3 @@ public class Decoration : Base, IInteractive, IBreakable
         }
     }
 }
-class Program
-{
-    static void Main(string[] args)
-    {
-        Decoration coffeeMug = new Decoration("Coffee Mug", 2);
-
-        Console.WriteLine(coffeeMug.ToString());
-
-        coffeeMug.Interact();
-        coffeeMug.Break();
-        coffeeMug.Break();
-        coffeeMug.Interact();
-    }
-}
