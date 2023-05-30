6e17bda805f28703585ed888fdb350a3245004e5
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-30 05:38:14 -0500

    task 1

---
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+public abstract class Base
+{
+    public string name;
+
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType()}";
+    }
+
+}
+
+public interface IInteractive
+{
+    void Interact();
+}
+
+public interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+public interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+public class TestObject : Base, IBreakable, IInteractive, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+    public void Break()
+    {
+        // provide implementation
+    }
+
+    public void Collect()
+    {
+        // provide implementation
+    }
+
+    public void Interact()
+    {
+        // provide implementation
+    }
+}
