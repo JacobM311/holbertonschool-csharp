6cbf9fc7e1e6a174354d8717e9184f2830ab4468
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-30 00:33:36 -0500

    task 4

--- csharp-generics/4-print/queue.cs
+++ csharp-generics/4-print/queue.cs
@@ -91,21 +91,3 @@ class Queue<T>
         }
     }
 }
-class Program
-{
-    static void Main(string[] args)
-    {
-        Queue<string> myStrQ = new Queue<string>();
-
-        myStrQ.Enqueue("hello");
-        myStrQ.Enqueue("holberton");
-        myStrQ.Enqueue("school");
-
-        myStrQ.Print();
-
-        Console.WriteLine("----------");
-
-        Queue<int> myIntQ = new Queue<int>();
-        myIntQ.Print();
-    }
-}
