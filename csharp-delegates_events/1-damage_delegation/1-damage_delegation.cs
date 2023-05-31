534302da8c0d181fe987004b6d184a43588fd2be
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-31 16:25:50 -0500

    task 1

---
+++ csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class Player
+{
+    private string name { get; set; }
+    private float maxHp { get; set; }
+    public float hp { get; private set; }
+    public delegate void CalculateHealth(float amount);
+
+    public Player() : this("Player", 100f){}
+    public Player(string name) : this(name, 100f){}
+    public Player(float maxHp) : this("Player", maxHp){}
+
+    public Player(string name, float maxHp)
+    {
+        if (maxHp <= 0)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            maxHp = 100f;
+        }
+        this.name = name;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0)
+        {
+            Console.WriteLine($"{name} takes 0 damage!");
+        }
+        else
+        {
+            Console.WriteLine($"{name} takes {damage} damage!");
+        }
+    }
+
+    public void HealDamage(float heal)
+    {
+        if (heal < 0)
+        {
+            Console.WriteLine($"{name} heals 0 HP!");
+        }
+        else
+        {
+            Console.WriteLine($"{name} heals {heal} HP!");
+        }
+    }
+}
