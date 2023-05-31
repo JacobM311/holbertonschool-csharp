8edb34c7a1d59b735b061376cee429f942e52006
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-31 15:09:52 -0500

    task 0

--- csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -25,15 +25,3 @@ public class Player
         Console.WriteLine($"{name} has {hp} / {maxHp} health");
     }
 }
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Player player1 = new Player("Electric Mouse", 500f);
-        Player player2 = new Player("Water Turtle");
-
-        player1.PrintHealth();
-        player2.PrintHealth();
-    }
-}
