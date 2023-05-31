0bb794b2305eb3dc031257a4f0b8dc3d76821bbe
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-31 15:18:57 -0500

    task 0

--- csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -8,12 +8,14 @@ public class Player
 
     public Player() : this("Player", 100f){}
     public Player(string name) : this(name, 100f){}
+    public Player(float maxHp) : this("Player", maxHp){}
 
     public Player(string name, float maxHp)
     {
         if (maxHp <= 0)
         {
             Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            maxHp = 100f;
         }
         this.name = name;
         this.maxHp = maxHp;
@@ -23,5 +25,14 @@ public class Player
     public void PrintHealth()
     {
         Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Player player = new Player(maxHp: -727f);
+
+        player.PrintHealth();
     }
 }
