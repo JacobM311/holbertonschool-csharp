1a7c7f4f119cc99857fcf11d7e47634f713776e6
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-05-31 19:47:00 -0500

    task 4

---
+++ csharp-delegates_events/4-check_yourself/4-check_yourself.cs
@@ -0,0 +1,163 @@
+﻿using System;
+
+public enum Modifier{
+    Weak,
+    Base,
+    Strong
+}
+
+public delegate float CalculateModifier(float baseValue, Modifier modifier);
+
+public class Player
+{
+    private string name { get; set; }
+    private float maxHp { get; set; }
+    public float hp { get; set; }
+    private string status { get; set; }
+
+    // HPCheck event
+    public event EventHandler<CurrentHPArgs> HPCheck;
+
+    public Player(string name="Player", float maxHp=100f)
+    {
+        if (maxHp <= 0)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            maxHp = 100f;
+        }
+        this.name = name;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+        this.status = $"{name} is ready to go!";
+        this.HPCheck += CheckStatus;
+    }
+
+    public float ApplyModifier(float baseValue, Modifier modifier)
+    {
+        if (modifier == Modifier.Weak)
+        {
+            return baseValue/2f;
+        }
+        else if (modifier == Modifier.Base)
+        {
+            return baseValue;
+        }
+        else
+        {
+            return baseValue * 1.5f;
+        }
+    }
+
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0)
+        {
+            Console.WriteLine($"{name} takes 0 damage!");
+        }
+        else
+        {
+            Console.WriteLine($"{name} takes {damage} damage!");
+            hp = hp - damage;
+            ValidateHP(hp);
+        }
+    }
+
+    public void HealDamage(float heal)
+    {
+        if (heal < 0)
+        {
+            Console.WriteLine($"{name} heals 0 HP!");
+        }
+        else
+        {
+            Console.WriteLine($"{name} heals {heal} HP!");
+            hp = hp + heal;
+            ValidateHP(hp);
+        }
+    }
+
+    public void ValidateHP(float newHp)
+    {
+        if (newHp < 0)
+        {
+            hp = 0;
+        }
+        else if (newHp > maxHp)
+        {
+            hp = maxHp;
+        }
+        else
+        {
+            hp = newHp;
+        }
+
+        // Trigger the HPCheck event
+        HPCheck?.Invoke(this, new CurrentHPArgs(hp));
+    }
+
+    // CheckStatus method
+    private void CheckStatus(object sender, CurrentHPArgs e)
+    {
+        if (e.currentHp == maxHp)
+        {
+            status = $"{name} is in perfect health!";
+        }
+        else if (e.currentHp >= maxHp / 2)
+        {
+            status = $"{name} is doing well!";
+        }
+        else if (e.currentHp >= maxHp / 4)
+        {
+            status = $"{name} isn't doing too great...";
+        }
+        else if (e.currentHp > 0)
+        {
+            status = $"{name} needs help!";
+        }
+        else
+        {
+            status = $"{name} is knocked out!";
+        }
+
+        Console.WriteLine(status);
+    }
+}
+
+
+public class CurrentHPArgs : EventArgs
+{
+    public float currentHp { get; }
+
+    public CurrentHPArgs(float newHp)
+    {
+        newHp = currentHp;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Player player = new Player("Electric Mouse");
+        CalculateModifier mod = new CalculateModifier(player.ApplyModifier);
+
+        player.PrintHealth();
+        Console.WriteLine();
+
+        player.TakeDamage(mod(75f, Modifier.Base));
+        player.PrintHealth();
+        Console.WriteLine();
+
+        player.HealDamage(mod(25f, Modifier.Base));
+        player.PrintHealth();
+        Console.WriteLine();
+
+        player.TakeDamage(mod(50f, Modifier.Base));
+        player.PrintHealth();
+    }
+}
