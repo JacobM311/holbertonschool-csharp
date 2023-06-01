3b5b59ffe75345d6271af44509a2543542c23030
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-06-01 10:30:26 -0500

    task 0

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,15 +1,36 @@
-namespace MyMath.Tests;
+using NUnit.Framework;
 
-public class Tests
+namespace MyMath.Tests
 {
-    [SetUp]
-    public void Setup()
+    [TestFixture]
+    public class Tests
     {
-    }
+        [Test]
+        public void AddEquals()
+        {
+            int result = MyMath.Operations.Add(2, 2);
+            Assert.AreEqual(4, result);
+        }
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+        [Test]
+        public void AddNegative()
+        {
+            int result = MyMath.Operations.Add(-2, -1);
+            Assert.AreEqual(-3, result);
+        }
+
+        [Test]
+        public void AddNegativePositive()
+        {
+            int result = MyMath.Operations.Add(-1, 2);
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void AddZero()
+        {
+            int result = MyMath.Operations.Add(0, 2);
+            Assert.AreEqual(2, result);
+        }
     }
 }
