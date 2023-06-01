9c3d3aab5ef1e75e6f1fe236d321fd0df2bba6dc
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-06-01 10:35:32 -0500

    task 0

--- csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -1,15 +1,68 @@
-namespace MyMath.Tests;
+using NUnit.Framework;
+using MyMath;
 
-public class Tests
+namespace MyMath.Tests
 {
-    [SetUp]
-    public void Setup()
+    public class Tests
     {
-    }
+        [SetUp]
+        public void Setup()
+        {
+        }
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+        [Test]
+        public void DivideMatrixEven()
+        {
+            int [,] test = new int[,] {{2, 4}, {6, 8}};
+            int [,] expected = new int[,] {{1, 2}, {3, 4}};
+            int [,] result = Matrix.Divide(test, 2);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void DivideMatrixOdd()
+        {
+            int [,] test = new int[,] {{3, 5}, {7, 9}};
+            int [,] expected = new int[,] {{1, 2}, {3, 4}};
+            int [,] result = Matrix.Divide(test, 2);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void DivideMatrixZero()
+        {
+            int [,] test = new int[,] {{3, 5}, {7, 9}};
+            int [,] result = Matrix.Divide(test, 0);
+
+            Assert.IsNull(result);
+        }
+        [Test]
+        public void DivideMatrixNull()
+        {
+            int [,] result = Matrix.Divide(null, 2);
+
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void DivideMatrixNegativeBy2()
+        {
+            int [,] test = new int[,] {{-2, -4}, {-6, -8}};
+            int [,] expected = new int[,] {{-1, -2}, {-3, -4}};
+            int [,] result = Matrix.Divide(test, 2);
+
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void DivideMatrixNumberByNegative()
+        {
+            int [,] test = new int[,] {{2, 4}, {6, 8}};
+            int [,] expected = new int[,] {{-1, -2}, {-3, -4}};
+            int [,] result = Matrix.Divide(test, -2);
+
+            Assert.AreEqual(expected, result);
+        }
     }
 }
