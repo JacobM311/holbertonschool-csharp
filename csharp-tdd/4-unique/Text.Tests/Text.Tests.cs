3a9bc9cd5cd00f7999ead2ffb40ec4d37c4c7fbc
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-06-01 10:50:35 -0500

    task 0

--- csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
+++ csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -1,15 +1,56 @@
-namespace Text.Tests;
+using NUnit.Framework;
+using Text;
 
-public class Tests
+namespace Text.Tests
 {
-    [SetUp]
-    public void Setup()
+	[TestFixture]
+    public class Tests
     {
-    }
+        [Test]
+        public void Unique()
+        {
+			string test = "uniq";
+			int result = Str.UniqueChar(test);
+            Assert.AreEqual(0, result);
+        }
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+        [Test]
+        public void UniqueNotUnique()
+        {
+			string test = "hiahiahia";
+			int result = Str.UniqueChar(test);
+            Assert.AreEqual(-1, result);
+        }
+
+        [Test]
+        public void UniqueEmpty()
+        {
+			string test = "";
+			int result = Str.UniqueChar(test);
+            Assert.AreEqual(-1, result);
+        }
+
+        [Test]
+        public void UniqueOneChar()
+        {
+			string test = "h";
+			int result = Str.UniqueChar(test);
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void UniqueLast()
+        {
+			string test = "hihililia";
+			int result = Str.UniqueChar(test);
+            Assert.AreEqual(8, result);
+        }
+
+        [Test]
+        public void UniqueNull()
+        {
+			int result = Str.UniqueChar(null);
+            Assert.AreEqual(-1, result);
+        }
     }
 }
