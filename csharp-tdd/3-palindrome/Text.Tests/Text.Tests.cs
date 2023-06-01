c8bdbaed4f972439d7cceb8bb779e00f4d10906f
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-06-01 10:46:36 -0500

    task 0

--- csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
+++ csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -1,15 +1,85 @@
-namespace Text.Tests;
+using NUnit.Framework;
 
-public class Tests
+namespace Text.Tests
 {
-    [SetUp]
-    public void Setup()
+    public class Tests
     {
-    }
+        [SetUp]
+        public void Setup()
+        {
+        }
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+        [Test]
+        public void TestTrue()
+        {
+            string test = "beeb";
+            bool result = Str.IsPalindrome(test);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void TestFalse()
+        {
+            string test = "boeb";
+            bool result = Str.IsPalindrome(test);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void TestSpecialChar()
+        {
+            string test = "A man, a plan, a canal: Panama.";
+            bool result = Str.IsPalindrome(test);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void EmptyString()
+        {
+            string test = "";
+            bool result = Str.IsPalindrome(test);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void TestUppercase()
+        {
+            string test = "Racecar";
+            bool result = Str.IsPalindrome(test);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void TestNumber()
+        {
+            string test = "01010";
+            bool result = Str.IsPalindrome(test);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void TestNumberAndCharFalse()
+        {
+            string test = "01No010";
+            bool result = Str.IsPalindrome(test);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void TestNumberAndCharTrue()
+        {
+            string test = "01No0on10";
+            bool result = Str.IsPalindrome(test);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void TestUppercaseFalse()
+        {
+            string test = "NoPalindoMe";
+            bool result = Str.IsPalindrome(test);
+            Assert.IsFalse(result);
+        }
+
     }
 }
