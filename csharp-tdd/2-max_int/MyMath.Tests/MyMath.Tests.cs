d217f70e04c0705ae592b9963ac4ed3036945215
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-06-01 10:39:49 -0500

    task 0

--- csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -1,15 +1,85 @@
-namespace MyMath.Tests;
+using NUnit.Framework;
+using System.Collections.Generic;
+using MyMath;
 
-public class Tests
+namespace MyMath.Tests
 {
-    [SetUp]
-    public void Setup()
+    public class Tests
     {
-    }
+        [SetUp]
+        public void Setup()
+        {
+        }
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+        [Test]
+        public void TestList()
+        {
+            List<int> test = new List<int>() {1, 2, 3, 4, 5, 6};
+            int result =  MyMath.Operations.Max(test);
+
+            Assert.AreEqual(result, 6);
+        }
+
+        [Test]
+        public void TestEmptyList()
+        {
+            List<int> test = new List<int>() {};
+            int result =  MyMath.Operations.Max(test);
+
+            Assert.AreEqual(result, 0);
+        }
+
+        [Test]
+        public void TestEqualList()
+        {
+            List<int> test = new List<int>() {1, 1, 1, 1, 1};
+            int result =  MyMath.Operations.Max(test);
+
+            Assert.AreEqual(result, 1);
+        }
+
+        [Test]
+        public void TestNegativeList()
+        {
+            List<int> test = new List<int>() {-1, -2, -3, -4, -5, -6};
+            int result =  MyMath.Operations.Max(test);
+
+            Assert.AreEqual(result, -1);
+        }
+
+        [Test]
+        public void TestNegativeAndPositiveList()
+        {
+            List<int> test = new List<int>() {-1, 2, -3, 4, -5, -6};
+            int result =  MyMath.Operations.Max(test);
+
+            Assert.AreEqual(result, 4);
+        }
+
+        [Test]
+        public void TestOneElement()
+        {
+            List<int> test = new List<int>() {-6};
+            int result =  MyMath.Operations.Max(test);
+
+            Assert.AreEqual(result, -6);
+        }
+
+        [Test]
+        public void TestGreaterIsZero()
+        {
+            List<int> test = new List<int>() {-5, -4, -3, -2, -1, 0};
+            int result =  MyMath.Operations.Max(test);
+
+            Assert.AreEqual(result, 0);
+        }
+
+        [Test]
+        public void TestNull()
+        {
+            int result =  MyMath.Operations.Max(null);
+
+            Assert.AreEqual(result, 0);
+        }
     }
 }
