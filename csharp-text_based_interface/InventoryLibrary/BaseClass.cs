f25a71a88541cfd0ac1de47fb3b4d442d425d8af
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-06-02 07:19:12 -0500

    finish

--- csharp-text_based_interface/InventoryLibrary/BaseClass.cs
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-public class Class1
+public class BaseClass
 {
     public string id;
     public DateTime date_created;
---
+++ csharp-text_based_interface/InventoryManagement.Tests/InventoryManagement.Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InventoryManagement.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
